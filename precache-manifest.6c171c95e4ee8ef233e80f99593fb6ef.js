f95811ed6561c30598b99ce66c72482b1b5035ff
Slugalisk <slugalisk@gmail.com>
2019-03-07 20:36:54 -0800

    Updates

---
+++ precache-manifest.6c171c95e4ee8ef233e80f99593fb6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de17361cd9c1532b737bb3e2345766ad",
+    "url": "/player/static/media/nodessl.de17361c.node"
+  },
+  {
+    "revision": "ee527bd80be7ba1d8958",
+    "url": "/player/static/js/runtime~main.ee527bd8.js"
+  },
+  {
+    "revision": "e6ea0f50aded73d6220c",
+    "url": "/player/static/js/main.e6ea0f50.chunk.js"
+  },
+  {
+    "revision": "a5f0dada78b9645df45d",
+    "url": "/player/static/js/2.a5f0dada.chunk.js"
+  },
+  {
+    "revision": "e6ea0f50aded73d6220c",
+    "url": "/player/static/css/main.1c77201a.chunk.css"
+  },
+  {
+    "revision": "faaa4c1239f2764a9c12fe8d93e46f9f",
+    "url": "/player/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/player/precache-manifest.7ea2fea50c0061af09eb50a96231e481.js"
+  "/player/precache-manifest.6c171c95e4ee8ef233e80f99593fb6ef.js"
 );
 
 workbox.clientsClaim();
